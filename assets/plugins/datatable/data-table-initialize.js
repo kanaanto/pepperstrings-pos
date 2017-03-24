0f06908e02e4ad9ec89cfe25134b835749548e30
Nicklaus <roynicklaus@gmail.com>
2017-03-24 11:00:16 +0800

    Inventory View
    UI for the inventory view

---
+++ assets/plugins/datatable/data-table-initialize.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('table.display').DataTable();
+    $('table.display').addClass("table");
+    $('table.display').addClass("table-bordered");
+    $('table.display').addClass("table-hover");
+} );
